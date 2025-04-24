2a41c299fcc5d883e992b31198a681860b963877
kavirajkv <kavirajk36kv@gmail.com>
2025-04-24 18:45:47 +0530

    env fetching for prod added

--- main.go
+++ main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"fmt"
 	"go-verify/routes"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
 
 func loadenv() {
-	err:=godotenv.Load(".env")
-	if err != nil {
-		fmt.Println("Error loading .env file")
-	} else {
-		fmt.Println("Loaded .env file")
+	if os.Getenv("APP_ENV")!= "production" {
+		fmt.Println("Loading .env file")
+		err:=godotenv.Load(".env")
+		if err != nil {
+			fmt.Println("Error loading .env file")
+		} else {
+			fmt.Println("Loaded .env file")
+		}
+	}else{
+		fmt.Println("Production environment detected, skipping .env loading")
 	}
 }
 	
